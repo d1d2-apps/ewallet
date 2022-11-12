22fd60c8d531551ce9098ccf51a2d50b3e263722
Danilo Ferreira <danilo1705@gmail.com>
2022-11-12 17:33:29 -0300

    update: prepare for deploy

---
+++ dist/main.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const common_1 = require("@nestjs/common");
+const core_1 = require("@nestjs/core");
+const app_module_1 = require("./app.module");
+const firebase_config_1 = require("./config/firebase.config");
+(0, firebase_config_1.initializeFirebaseApp)();
+async function bootstrap() {
+    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.useGlobalPipes(new common_1.ValidationPipe());
+    await app.listen(3333);
+}
+bootstrap();
+//# sourceMappingURL=main.js.map
