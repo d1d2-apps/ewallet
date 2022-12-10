38b3cae6cd54b0593d720804094ec46cee0404d7
Danilo Ferreira <danilo1705@gmail.com>
2022-12-10 19:07:33 -0300

    refactor: remove test folder

--- src/app.e2e.spec.ts
+++ src/app.e2e.spec.ts
@@ -1,12 +1,13 @@
+import request from 'supertest';
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+
+import { AppModule } from './app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -15,7 +16,13 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer()).get('/').expect(200).expect('Hello World!');
+  describe('root', () => {
+    it('should return welcome message', () => {
+      return request(app.getHttpServer()).get('/').expect(200).expect({ message: 'Welcome to the eWallet API' });
+    });
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
