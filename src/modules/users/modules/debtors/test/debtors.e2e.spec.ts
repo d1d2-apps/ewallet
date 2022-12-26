792cd5129ff7144343eee68f568aa50d74bd4a08
Danilo Ferreira <danilo1705@gmail.com>
2022-12-26 19:39:40 -0300

    chore: finish create debtors tests

--- src/modules/users/modules/debtors/test/debtors.e2e.spec.ts
+++ src/modules/users/modules/debtors/test/debtors.e2e.spec.ts
@@ -9,6 +9,7 @@ import { AuthService, IAuthResponse } from '@src/modules/auth/auth.service';
 import { mockRandomEmail, mockRandomInvalidToken, mockRandomName, mockRandomPassword, mockRandomUuid } from '@src/utils/tests/mocks.fn';
 import { DebtorsService } from '../debtors.service';
 import { DebtorModel } from '../models/debtor.model';
+import { mockCreateDebtorNoDataResponse } from './mocks/debtors-responses.mock';
 
 describe('DebtorsController (e2e)', () => {
   let app: INestApplication;
@@ -115,17 +116,61 @@ describe('DebtorsController (e2e)', () => {
     it('should not get debtor with invalid ID', async () => {
       const invalidDebtorId = mockRandomUuid();
 
-      const response = await api.get(`/users/debtors/${invalidDebtorId}`).set('authorization', `Bearer ${userAuth.token}`).expect(400);
+      const response = await api.get(`/users/debtors/${invalidDebtorId}`).set('authorization', `Bearer ${userAuth.token}`).expect(404);
 
       expect(response.body.message).toStrictEqual(`Debtor not found with id [${invalidDebtorId}]`);
     });
 
     it('should get debtor by ID', async () => {
-      const response = await api.get(`/users/debtors/${debtor.id}`).set('authorization', `Bearer ${userAuth.token}`).expect(400);
+      const response = await api.get(`/users/debtors/${debtor.id}`).set('authorization', `Bearer ${userAuth.token}`).expect(200);
 
       expect(response.body.id).toStrictEqual(debtor.id);
       expect(response.body.color).toStrictEqual(debtor.color);
       expect(response.body.name).toStrictEqual(debtor.name);
+    });
+  });
+
+  describe('create debtor', () => {
+    it('should raise 400 for no data', async () => {
+      const response = await api.post('/users/debtors').expect(401);
+
+      expect(response.body.message).toStrictEqual('JWT token is missing');
+    });
+
+    it('should raise 400 for no data', async () => {
+      const invalidToken = mockRandomInvalidToken();
+
+      const response = await api.post('/users/debtors').set('authorization', `Bearer ${invalidToken}`).expect(401);
+
+      expect(response.body.message).toStrictEqual('Invalid JWT token');
+    });
+
+    it('should raise 400 for no data', async () => {
+      const response = await api.post('/users/debtors').set('authorization', `Bearer ${userAuth.token}`).send(undefined).expect(400);
+
+      expect(response.body.message).toStrictEqual(mockCreateDebtorNoDataResponse);
+    });
+
+    it('should raise 400 for not hexadecimal color', async () => {
+      const data = {
+        name: mockRandomName(),
+        color: 'white',
+      };
+
+      await api.post('/users/debtors').set('authorization', `Bearer ${userAuth.token}`).send(data).expect(400);
+    });
+
+    it('should create debtor', async () => {
+      const data = {
+        name: mockRandomName(),
+        color: '#ffffff',
+      };
+
+      const response = await api.post('/users/debtors').set('authorization', `Bearer ${userAuth.token}`).send(data).expect(201);
+
+      expect(response.body.id).toBeDefined();
+      expect(response.body.name).toStrictEqual(data.name);
+      expect(response.body.color).toStrictEqual(data.color);
     });
   });
 });
---
+++ src/modules/users/modules/debtors/test/mocks/debtors-responses.mock.ts
@@ -0,0 +1,7 @@
+export const mockCreateDebtorNoDataResponse = [
+  'name should not be empty',
+  'name must be a string',
+  'color must be a hexadecimal color',
+  'color should not be empty',
+  'color must be a string',
+];
